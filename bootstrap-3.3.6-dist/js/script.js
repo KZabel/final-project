d106b5b3bd0ed6af89eb6c313fef510181427004
K. Zabel <kylee.zabel@gmail.com>
2016-06-08 21:35:31 -0700

    added slippy map, footer

--- bootstrap-3.3.6-dist/js/script.js
+++ bootstrap-3.3.6-dist/js/script.js
@@ -8,15 +8,8 @@
     height: '-=3'}, 'fast');
   }                           
   );
+  
 });*///Above code used for animating navbar <-- no longer in use
-
-//control carousel interval
-
-$(document).ready(function(){
-  $(".carousel").carousel({
-  interval: 15000
-});
-});
 
 
 /*//"Read more" links for RECENT NEWS section
@@ -38,10 +31,10 @@ function populateObject() {
 
 populateObject();*/
 
-//I'm not sure why this won't work
-$(function(){
-  $('.excerpt').each(function(evt){
-    var maxLength = 25;
+//I'm not sure why this won't work, but this was my first attempt at a "read more" link.
+/*$(document).ready(function(){
+  $('excerpt').each(function(evt){
+    var maxLength = 3;
     if($(this).html().length > maxLength){
       var before = $(this).html().substr(0, maxLength);
       var after = $(this).html().substr(maxLength);
@@ -50,6 +43,64 @@ $(function(){
         evt.preventDefault();
         $(this).hide();
         $(this).parents('.excerpt').find('additional-text').show();
-      });    
+      }); 
+    }
+  }
+});*/
+
+/*-----------------------------------------------------Read more------------------------------------------------------*/
+
+$(document).ready(function(){
+  var maxLength = 160;
+  var ellipses = "...";
+  var readMore = "Read more";
+  var readLess = "Read less";
+  
+  $('.excerpt').each(function(){
+    var content = $(this).html();
+    if(content.length > maxLength){
+      var i = content.substr(0, maxLength);
+      var j = content.substr(maxLength, content.length - maxLength);
+      var html = i + '<span class="ellipses">' + ellipses + '&nbsp;</span><span class="additional-text"><span>' + j + '</span>&nbsp;&nbsp;<a href="#" class="link">' + readMore + '</a></span>';
+      $(this).html(html);
+    }
   });
+  
+  $('.link').click(function(){
+    if($(this).hasClass("less")){
+      $(this).removeClass("less");
+      $(this).html(readMore);
+    } else{
+      $(this).addClass("less");
+      $(this).html(readLess);
+    }
+    
+    $(this).parent().prev().toggle();
+    $(this).prev().toggle();
+    return false;
+    
+  });
+  
 });
+
+/*-----------------------------------------------------Google Map-----------------------------------------------------*/
+    
+function initMap(){
+  var myLatLng = {lat: 47.035722, lng: -122.904830};
+  var mapDiv = document.getElementById('map');
+  var map = new google.maps.Map(mapDiv, {center: myLatLng, zoom: 16});
+  var marker = new google.maps.Marker({
+        position: {lat: 47.035722, lng: -122.904830}
+    });
+
+marker.setMap(map);
+
+var infoWindow = new google.maps.InfoWindow({
+        content: 'Gov Comms'
+    });
+marker.addListener( 'click', function() {
+        infoWindow.open(map, marker);
+    });
+};
+
+
